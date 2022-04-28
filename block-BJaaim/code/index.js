56e9558fc4aa51403fa468c4a96451400532daea
ravindrapv <ravindra.ravi4849@gmail.com>
2022-04-29 01:09:16 +0530

    quiz  app

---
+++ block-BJaaim/code/index.js
@@ -0,0 +1,10 @@
+class quiz{
+    constructor(Title,Options,Correct){
+        this.Title = Title;
+        this.Options = Options;
+        this.Correct = Correct;
+    }
+    isCorrect(answer){
+
+    }
+}
